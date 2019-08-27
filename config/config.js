72fb8489ee094a6bc0fbd41c8450453ec1b7b035
Olushola <olushola@Users-MacBook-Pro.local>
2019-08-27 11:46:43 +0100

    configuration file bug fixed

--- config/config.js
+++ config/config.js
@@ -26,7 +26,7 @@ module.exports = {
         }
     },
     "production": {
-        "url": process.env.DB_USERNAME,
+        "username": process.env.DB_USERNAME,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_DATABASE,
         "host": process.env.DB_HOST,
