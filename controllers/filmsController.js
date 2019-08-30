8f5ec9b75f11388f619140dadc4985270782ae70
Olushola <olushola@Users-MacBook-Pro.local>
2019-08-30 14:33:46 +0100

    sending all film data along with response

--- controllers/filmsController.js
+++ controllers/filmsController.js
@@ -1,6 +1,6 @@
 const Films = require("../models").Film
 const { FILMS } = require("../logic/constants")
-const { sortResponse, retrieveData, extractFields } = require("../logic/helpers")
+const { sortResponse, retrieveData } = require("../logic/helpers")
 const { attachCommentsCount } = require("../logic/filmLogic")
 const { sendSuccessResponse, sendFailureResponse } = require("../logic/response")
 
@@ -10,8 +10,7 @@ exports.getFilms = async (req, res) => {
         if (!films.length) return sendFailureResponse(res, "No film found", [], 404)
 
         const film_data = await Films.findAll()
-        const trimmed_films = extractFields(films, ["episode_id", "title", "release_date", "opening_crawl"])
-        const films_with_comments = attachCommentsCount(trimmed_films, film_data)
+        const films_with_comments = attachCommentsCount(films, film_data)
         const response = films_with_comments.sort(sortResponse("release_date", "asc"))
 
         return sendSuccessResponse(res, "Success", response, 200)
