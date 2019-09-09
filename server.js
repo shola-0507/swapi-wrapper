a535b904c546243dd60a218bdc6cd8cbffc46d6f
Olushola <olushola@Users-MacBook-Pro.local>
2019-09-09 08:13:26 +0100

    final changes

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-unused-vars */
 require("dotenv").config() // configure environment variables
 const models = require("./models")
+const { fetchAndSaveDataToRedis } = require("./logic/helpers")		
+const { FILMS, PEOPLE } = require("./logic/constants")
 
 models.sequelize.sync().then(() => {
     console.log("Database migrated!")
@@ -12,6 +15,18 @@ api.set("port", process.env.PORT)
 
 const server = api.listen(api.get("port"), () => {
     console.log(`Api server running on PORT ${server.address().port}`)
+})
+
+// eslint-disable-next-line no-unexpected-multiline
+server.on("listening", async () => {
+    try {		
+        const films = await fetchAndSaveDataToRedis(FILMS)		
+        const people = await fetchAndSaveDataToRedis(PEOPLE)		
+    
+        console.log("Saved films and character data to redis")		
+    } catch (error) {		
+        console.log("something went wrong saving data to redis..." + error.message)		
+    }
 })
 
 module.exports = server
